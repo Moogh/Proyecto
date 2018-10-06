aa92e0ed0e72d2cb695a66642fbdc3579a51a0d6
SalomónGrc <salomon.grc@gmail.com>
2018-10-06 12:05:12 -0500

    Update ProyectoDesarrollo.java
    Se hace un cambio desde la web

--- ProyectoDesarrollo/src/proyectodesarrollo/ProyectoDesarrollo.java
+++ ProyectoDesarrollo/src/proyectodesarrollo/ProyectoDesarrollo.java
@@ -15,7 +15,7 @@ public class ProyectoDesarrollo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola mundo dos");
+        System.out.println("Hola mundo dos prueba desde la web");
     }
     
 }
